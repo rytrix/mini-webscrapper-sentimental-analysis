bfcf4faede8c449aa1b89f2129e1dcfe74f8c57b
Ryan <rmw071102@gmail.com>
2025-03-22 11:30:43 -0400

    cli interface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-
+import sys
 
 def download_website(url):
     headers = {
@@ -24,10 +24,12 @@ def seperate_headers_paragraphs(website):
 
     return extracted_text
 
-def main():
-    website = download_website("https://www.coindesk.com/markets/2025/03/17/crypto-whale-shorts-usd445m-in-btc-while-taking-bullish-bet-on-melania-token-hyperliquid-data-show")
+def main(url):
+    website = download_website(url)
     content = seperate_headers_paragraphs(website)
     print(content)
     
-
-main()
+if __name__ == "__main__":
+    args = sys.argv      
+    if len(args) >= 2:
+        main(args[1])
