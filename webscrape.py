f1bd259b26e946030c7eddb4f13ff38872183dd2
Ryan <rmw071102@gmail.com>
2025-03-27 15:21:22 -0400

    modified formatting

--- webscrape.py
+++ webscrape.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 
+
 def download_website(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.3"}
@@ -24,12 +25,14 @@ def seperate_headers_paragraphs(website):
 
     return extracted_text
 
+
 def main(url):
     website = download_website(url)
     content = seperate_headers_paragraphs(website)
     print(content)
-    
+
+
 if __name__ == "__main__":
-    args = sys.argv      
+    args = sys.argv
     if len(args) >= 2:
         main(args[1])
