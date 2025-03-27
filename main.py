a2b9ca5f1dc878323948f042c4814b222f99fc85
Ryan <rmw071102@gmail.com>
2025-03-27 15:20:03 -0400

    added better system prompt for sentimental analysis

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ def sentiment_analysis(content):
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=content,
-        config = genai.types.GenerateContentConfig(system_instruction="Perform sentimental analysis on this scraped website")
+        config=genai.types.GenerateContentConfig(
+            system_instruction="Perform sentimental analysis on this scraped website. Give a short 3-5 sentence summary, a score from 0-10 (negative to positive), and answer the following questions. What led to you giving this article the score you gave it? What emotions are present in the article?")
     )
     print(response.text)
 
@@ -23,8 +24,8 @@ def main(url):
     print(str(content))
     sentiment_analysis(str(content))
 
-    
+
 if __name__ == "__main__":
-    args = sys.argv      
+    args = sys.argv
     if len(args) >= 2:
         main(args[1])
